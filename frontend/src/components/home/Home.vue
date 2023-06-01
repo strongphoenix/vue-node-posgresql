d33faf6981a7ad461026dc74ea0133d232ad0f46
strongphoenix <pheonix20210101@gmail.com>
2023-06-02 04:48:42 +1000

    node vue postgresql

---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="home"> 
+        <PageTitle icon="fa fa-home" main="Dashboard" 
+            sub="VueJS + Bootstrap-Vue + Vue-Gravatar + Vue-Toasted + Vue-Chartjs + Axios + JWT + Backend NodeJS + PostgreSQL" />
+        
+        <div class="stats">
+            <Stat title="Usuários Ativos" :value="stat.users" icon="fa fa-user" color="#3282cd" /> 
+        </div>
+
+        <ChartBar />
+    </div>
+</template>
+
+<script>
+import PageTitle from '../template/PageTitle'
+import Stat from './Stat'
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import ChartBar from '@/components/chart/ChartBar';
+
+export default {
+    name: 'Home',
+    components: { PageTitle, Stat, ChartBar },
+    data() {
+        return {
+            stat: {}
+        }
+    },
+    methods: {
+        getStats() {
+            axios.get(`${baseApiUrl}/users/stats`) 
+                 .then(resp => this.stat = resp.data)
+        }
+    },
+    mounted() {
+        this.getStats()
+    }
+}
+</script>
+
+<style>
+    .stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+</style>
---
+++ frontend/src/components/template/UserDropdown.vue
@@ -0,0 +1,95 @@
+<template>
+    <div class="user-dropdown">
+        <div class="user-button">
+            <span class="d-none d-sm-block">{{ user.name }}</span>
+            <div class="user-dropdown-img">
+                <Gravatar :email="user.email" alt="Usuário" />
+            </div>
+        </div>
+        <div class="user-dropdown-content">
+            <router-link to="/admin" v-if="user.admin"><i class="fa fa-user"></i> Admin</router-link>
+            <router-link to="/about"><i class="fa fa-address-card"></i> About</router-link>
+            <a href @click.prevent="logout"><i class="fa fa-sign-out"></i> Logout</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import { userCredential } from '@/global'
+import { mapState } from 'vuex'
+import Gravatar from 'vue-gravatar'
+
+export default {
+    name: 'UserDropdown',
+    components: { Gravatar },
+    computed: mapState(['user']),
+    methods: {
+        logout() {
+            localStorage.removeItem(userCredential)
+            this.$store.commit('setUser', null)
+            this.$router.push({ name: 'auth' })
+        }
+    }
+}
+</script>
+
+<style>
+    .user-dropdown {
+        position: relative;
+        height: 100%;
+        cursor: pointer;
+    }
+
+    .user-button {
+        display: flex;
+        align-items: center;
+        color: #fff;
+        font-weight: 100;
+        height: 100%;
+        padding: 0px 20px;
+    }
+
+    .user-dropdown-img {
+        margin: 0px 10px;
+    }
+
+    .user-dropdown-img > img {
+        max-height: 50px;
+        border-radius: 50px;
+    }
+
+    .user-dropdown-content {
+        position: absolute;
+        right: 10px;
+        background-color: #f9f9f9;
+        min-width: 180px;
+        box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+        padding: 10px;
+        z-index: 1;
+        
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        
+        visibility: hidden;     
+        opacity: 1;
+        transition: visibility 0s, opacity 0.5s linear; 
+    }
+
+    .user-dropdown:hover .user-dropdown-content {
+        visibility: visible;
+        opacity: 1; 
+    }
+
+    .user-dropdown-content a {
+        text-decoration: none;
+        color: #333;
+        padding: 10px;
+    }
+
+    .user-dropdown-content a:hover {
+        text-decoration: none;
+        color: #333;
+        background-color: #ededed;
+    }
+</style>
